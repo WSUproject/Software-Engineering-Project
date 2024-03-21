05ccf8029334fcfdcc90cb6bb587e75974174e51
ROHAN\laptop <krb.rohan@gmail.com>
2024-03-21 18:14:16 -0400

    Completed job listing view and job details (ToDo: Apply for job)

---
+++ frontend/src/components/Jobs/Job.js
@@ -0,0 +1,129 @@
+import React from "react";
+import axios from "axios";
+import "./Jobs.css";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Button from "@mui/material/Button";
+// import { makeStyles } from "@mui/styles";
+import JobDetails from "./JobDetailsModal";
+import { Link } from "react-router-dom";
+
+// const useStyles = makeStyles((theme) => ({
+//   button: {
+//     // backgroundColor: "#3dc21b",
+//     // color: "#000000",
+//     width: "200px",
+//     cursor: "pointer",
+//     // padding: "16px 31px",
+//     fontSize: "14px",
+//     "&:hover": {
+//       backgroundColor: "#88d2ec",
+//       color: "#000000",
+//     },
+//   },
+// }));
+
+const baseURL = "http://localhost:8000/jobs";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function JobCard() {
+  // const classes = useStyles();
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (job) => {
+    setAge(job.target.value);
+  };
+
+  const [jobpost, setjobPost] = React.useState(null);
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setjobPost(response.data);
+      // console.log(response)
+    });
+  }, []);
+
+  if (!jobpost) return null;
+
+  const jobs = jobpost.map((data) => {
+    return (
+      <div key={data.id}>
+        <div className="jobCard">
+          <img src={data.jobBanner} alt="myPic" className="jobCard_img" />
+
+          <div className="jobCard_info">
+            <h2 className="job_title">{data.post}</h2>
+
+            <h3 className="job_type">{data.type} </h3>
+            <h3 className="vacancy">Vacancy: {data.vacancy} </h3>
+            <h3 className="company">{data.institution_name} </h3>
+
+            <div>
+              {/* <button onClick={handleOpen}> View Details</button> */}
+              <JobDetails id={data.id} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+
+    {
+      /* </div> */
+    }
+  });
+
+  return (
+    <>
+      <div className="top">
+        {/* <Box sx={{ minWidth: 200 }} className="selectCategory">
+          <FormControl fullWidth>
+            <InputLabel id="select-label">Category</InputLabel>
+            <Select
+              labelId="select-label"
+              id="select"
+              value={age}
+              label="Category"
+              onChange={handleChange}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </Box> */}
+        <Button
+          // className="Createbtn"
+          variant="contained"
+          size="large"
+          color="primary"
+          type="submit"
+          // className="btnSubmit"
+          // className={classes.button}
+          component={Link}
+          to="./CreateJobForm"
+        >
+          Create Vacancy
+        </Button>
+      </div>
+      <div className="jobCards">{jobs}</div>
+    </>
+  );
+}
---
+++ frontend/src/components/Jobs/JobDetailsModal.js
@@ -0,0 +1,145 @@
+import React from "react";
+import axios from "axios";
+import Modal from "@mui/material/Modal";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { BsCalendarDate } from "react-icons/bs";
+import { BsCoin } from "react-icons/bs";
+import { BsClock } from "react-icons/bs";
+import Button from "@mui/material/Button";
+import "./JobModal.css";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 1000,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function JobDetails(props) {
+  const [jobDetails, setjobDetails] = React.useState(null);
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  // http://localhost:8000/jobs?id=1
+
+  React.useEffect(() => {
+    axios.get(`http://localhost:8000/jobs?id=${props.id}`).then((response) => {
+      setjobDetails(response.data);
+      console.log("Response", response);
+    });
+  }, []);
+
+  console.log("jobdetails", jobDetails);
+  console.log("asdasdasd", props.id);
+
+  // var names = 'Harry,John,Clark,Peter,Rohn,Alice';
+
+  if (!jobDetails) return null;
+
+  return (
+    <div>
+      <button onClick={handleOpen}> View Details</button>
+      <Modal open={open} onClose={handleClose} id={props.id}>
+        <Box sx={style}>
+          <div class="parents">
+            <div class="div1j">
+              <b>College/Company</b>
+            </div>
+            <div class="div2j">
+              <b>:</b>
+            </div>
+            <div class="div3j">{jobDetails[0].institution_name}</div>
+            <div class="div4j">
+              <b>Post</b>
+            </div>
+            <div class="div5j">
+              <b>:</b>
+            </div>
+            <div class="div6j"> {jobDetails[0].post}</div>
+            <div class="div7j">
+              <b>Job Level</b>
+            </div>
+            <div class="div8j">
+              <b>:</b>
+            </div>
+            <div class="div9j">{jobDetails[0].level} </div>
+            <div class="div10j">
+              <b>Vacancies</b>
+            </div>
+            <div class="div11j">
+              <b>:</b>
+            </div>
+            <div class="div12j">{jobDetails[0].vacancy} </div>
+            <div class="div13j">
+              <b>Employment Type</b>
+            </div>
+            <div class="div14j">
+              <b>:</b>
+            </div>
+            <div class="div15j"> {jobDetails[0].type}</div>
+            <div class="div16j">
+              <b>Salary</b>
+            </div>
+            <div class="div17j">
+              <b>:</b>
+            </div>
+            <div class="div18j">${jobDetails[0].salary} </div>
+            <div class="div19j">
+              <b>Appication Deadline</b>
+            </div>
+            <div class="div20j">
+              <b>:</b>
+            </div>
+            <div class="div21j">{jobDetails[0].deadline} </div>
+            <div class="div22j">
+              <b>Education Level</b>
+            </div>
+            <div class="div23j">
+              <b>:</b>
+            </div>
+            <div class="div24j">{jobDetails[0].education} </div>
+            <div class="div25j">
+              <b>Experience</b>
+            </div>
+            <div class="div26j">
+              <b>:</b>
+            </div>
+            <div class="div27j">{jobDetails[0].experience}</div>
+          </div>
+          <div class="div28j">
+            <div>
+              <b>Other Specifications</b>
+            </div>
+            <div>
+              <ul className="lists">
+                {jobDetails[0].specifications.split(",").map((value, index) => {
+                  return <li key={index}>{value}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+          <div class="div29j">
+            <div>
+              <b>Job Description</b>
+            </div>
+            <div>
+              <ul className="lists">
+                {jobDetails[0].description.split(",").map((value, index) => {
+                  return <li key={index}>{value}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
