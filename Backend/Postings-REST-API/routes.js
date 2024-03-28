4337d2c3636f35d33158c6e3a6011ab3c9033638
sauravsitoula <saaurav10@gmail.com>
2024-03-27 23:20:54 -0400

    Added the APIS for discussions in the accommodation postings

--- Backend/Postings-REST-API/routes.js
+++ Backend/Postings-REST-API/routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const jobRoutes = require("./routes/job.routes");
 const eventRoutes = require("./routes/event.routes");
 const accommodationRoutes = require("./routes/accommodation.routes");
+const discussionRoutes = require("./routes/discussion.routes");
 
 router.use("/jobs", jobRoutes);
 router.use("/events", eventRoutes);
 router.use("/accommodations", accommodationRoutes);
+router.use("/discussions", discussionRoutes);
 
 module.exports = router;
