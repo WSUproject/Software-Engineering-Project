e3aabc11bf0e9d392f8ad3c44fadc4825bf5fe30
sauravsitoula <saaurav10@gmail.com>
2024-04-06 12:45:01 -0400

    finalization of BE

--- Backend/Postings-REST-API/index.js
+++ Backend/Postings-REST-API/index.js
@@ -13,7 +13,6 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
 
 app.use("/auth", authRoutes);
 app.use(validateToken);
